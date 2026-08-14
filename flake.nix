59c60a0953bc83833474db0dda70e3d0063150be
Ryan Dioneda <er.dioneda@gmail.com>
2026-08-13 23:50:34 -0400

    Create initial flake test package

--- flake.nix
+++ flake.nix
@@ -9,10 +9,23 @@
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+
+    niqol = pkgs.rustPlatform.buildRustPackage {
+      pname = "niqol";
+      version = "0.1.0";
+
+      src = ./.;
+
+      cargoLock.lockFile = ./Cargo.lock;
+    };
   in
   {
     devShells.${system}.default =
       import ./shell.nix { inherit pkgs; };
+
+    packages.${system} = {
+      default = niqol;
+    };
   };
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    fs::OpenOptions,
+    io::Write,
+    thread,
+    time::Duration
+};
+
+fn main() -> std::io::Result<()> {
+    loop {
+        let mut file = OpenOptions::new()
+            .read(true)
+            .append(true)
+            .create(true)
+            .open("/tmp/niqol.log")?;
+        writeln!(file, "niqol hearthbeat...")?;
+        thread::sleep(Duration::from_secs(1));
+    }
 }
