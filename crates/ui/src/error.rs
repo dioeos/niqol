5950acb6ef0252db3fe2b7f5205b621e410ed5a9
Ryan Dioneda <er.dioneda@gmail.com>
2026-09-25 23:07:32 -0400

    [#5] Add model filtering to loaded marks from query

---
+++ crates/ui/src/error.rs
@@ -0,0 +1,36 @@
+use niqol_ipc::error::Error as IpcError;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("{self:?}")]
+    FailedIpcOperation(#[source] niqol_ipc::error::Error),
+
+    #[error("{self:?}")]
+    FailedIpcClientConnect(#[source] niqol_ipc::error::Error),
+
+    #[error("{self:?}")]
+    UnexpectedSocketConflict(#[source] niqol_ipc::error::Error),
+}
+
+impl From<niqol_ipc::error::Error> for Error {
+    fn from(value: IpcError) -> Self {
+        match value {
+            source @ (
+                IpcError::FailedSocketIO(_)
+                | IpcError::EmptyIpcResponse(_)
+                | IpcError::FailedIpcSerdeOperation(_)
+            ) => Self::FailedIpcOperation(source),
+
+            source @ (
+                IpcError::FailedToBindListenerToSocket(_)
+                | IpcError::FailedToConnectToIpcSocket(_)
+                | IpcError::FailedToFindXdgRuntimeDirVar
+            ) => Self::FailedIpcClientConnect(source),
+
+            source @ (
+                IpcError::UnexpectedQueryResponse
+                | IpcError::FailedToAcceptConnection(_)
+            ) => Self::UnexpectedSocketConflict(source)
+        }
+    }
+}
